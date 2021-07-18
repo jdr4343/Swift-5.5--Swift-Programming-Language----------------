4bc95933a32e90764f42e1127076e42e88bf3a1f
jdr4343 <jdr4343@naver.com>
2021-07-19 07:43:47 +0900

    Semicolons

---
+++ Swift5.5 TheBasic.playground/Pages/Semicolons.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//세미콜론 (Semicolons)
+
+//다른 많은 언어들과는 달리, 스위프트는 코드 각 구문 뒤에 세미콜론 (;) 을 붙이는 것이 필수가 아닌데, 물론 원한다면 그렇게 해도 되긴 합니다. 하지만, 구분된 여러 구문을 한 줄에 작성하려면 세미콜론 이 (are) 필수입니다:
+let myDoll = ["hunsu", "dino"]; for item in myDoll { print(item) } ; if myDoll.count < 0 { print("a lot of doll") } else { print(" ^^! ")  }
+//: [Next](@next)
