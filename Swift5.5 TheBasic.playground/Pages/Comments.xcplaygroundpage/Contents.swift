e9993c69606c54652134aa778e5b0405dc14e73f
jdr4343 <jdr4343@naver.com>
2021-07-19 07:36:48 +0900

    Constants and Variables

---
+++ Swift5.5 TheBasic.playground/Pages/Comments.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//Comments (주석)
+//스스로 기록하거나 기억을 되살리기 위해, 실행되지 않는 문장을 코드에 포함시키려면 ‘주석 (comments)’ 을 사용합니다. 스위프트 컴파일러는 코드를 컴파일할 때 주석을 무시합니다.
+//‘한-줄짜리 (single-line) 주석’ 은 ‘빗금 두 개 (two forward-slashes; //)’ 로 시작합니다
+
+/* 이것도 주석이지만
+ 여러 줄에 걸쳐 작성됩니다 */
+//: [Next](@next)
