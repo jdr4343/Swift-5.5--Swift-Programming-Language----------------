8e8c5bfd4282d9069c1af9fc7b9d19a82a200646
jdr4343 <jdr4343@naver.com>
2021-07-19 13:44:56 +0900

    Assignment Operator

--- Swift 5.5- Basic Operators.playground/Pages/Assignment Operator.xcplaygroundpage/Contents.swift
+++ Swift 5.5- Basic Operators.playground/Pages/Assignment Operator.xcplaygroundpage/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Assignment Operator (할당 연산자)
+//할당 연산자 (assignment operator) (a = b) 는 a 의 값을 b 의 값으로 초기화하거나 ‘갱신 (update)’ 합니다:
+
+let b = 10
+var a = 5
+a = b
+// a 는 이제 10 입니다.
+
+//할당의 오른쪽이 ‘다중 (multiple) 값’ 을 가진 ‘튜플’ 이면, 원소들을 한 번에 다중 상수나 변수로 분해할 수 있습니다:
+let (x, y) = (3, 3)
+
